1d6a373a982933bf4f9b0f74001e241f406274a0
uwuhhhj <lwj2315969251@gmail.com>
2025-08-06 04:28:41 +0900

    尝试监听进群消息并推送指定群聊

--- main.py
+++ main.py
@@ -1,24 +1,55 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+import asyncio
+import re
+from typing import Dict, List
+from astrbot import logger
+from astrbot.core import AstrBotConfig
+from astrbot.api.event import filter
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.core.message.components import Reply
+from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
+    AiocqhttpMessageEvent,
+)
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register(
+    "astrbot_plugin_qqhelper",
+    "Loliiiico",
+    "[仅aiocqhttp] 群管助手",
+    "v1.0.0",
+    "https://github.com/uwuhhhj/astrbot_plugin_qqhelper",
+)
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
-
+        self.config = config
+        self.simmc_group: List[str] = config.get(
+            "simmc_group", []
+        )  # 启用此插件的simmc群聊
+        self.admin_group: List[str] = config.get(
+            "admin_group", []
+        )  # 管理员群聊
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        """初始化插件"""
+        pass
+    @filter.platform_adapter_type(filter.PlatformAdapterType.AIOCQHTTP)
+    async def event_monitoring(self, event: AiocqhttpMessageEvent):
+        """监听进群/退群事件"""
+        raw = getattr(event.message_obj, "raw_message", None)
+        if not isinstance(raw, dict):
+            return
+        client = event.bot
+        # 进群申请事件
+        if (
+                event.get_group_id() in self.simmc_group
+                and raw.get("post_type") == "request"
+                and raw.get("request_type") == "group"
+                and raw.get("sub_type") == "add"
+        ):
+            user_id = str(raw.get("user_id", ""))
+            group_id = str(raw.get("group_id", ""))
+            comment = raw.get("comment")
+            flag = raw.get("flag", "")
+            nickname = (await client.get_stranger_info(user_id=int(user_id)))[
+                           "nickname"
+                       ] or "未知昵称"
+            reply = f"【收到进群申请】同意进群吗：\n昵称：{nickname}\nQQ：{user_id}\nflag：{flag}"
+            await client.send_group_msg(group_id=int(self.admin_group[0]), message=reply)
